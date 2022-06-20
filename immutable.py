af7a729e7bc527cfe78e39e301d1af4b3843b096
yujiakai8888 <37033864+yujiakai8888@users.noreply.github.com>
2022-06-20 20:25:38 +0800

    6.20

--- immutable.py
+++ immutable.py
@@ -1,3 +1,4 @@
+import copy
 class HashMap(object):
 
     def __init__(self, size=5):
@@ -7,114 +8,6 @@ class HashMap(object):
         self.keynumber = 0
         for i in range(size):
             self.data.append([])
-
-    # 1. add
-    def add(self, key):
-        new = HashMap()
-        new.data = self.data
-        new.len = self.len
-        new.keynumber = self.keynumber
-        new.__curr__ = self.__curr__
-
-        if new.is_member(key):
-            return new
-        index = key % new.len
-        new.data[index].append(key)
-        new.keynumber += 1
-        return new
-
-    # 2. remove
-    def remove(self, key):
-        new = HashMap()
-        new.data = self.data
-        new.len = self.len
-        new.keynumber = self.keynumber
-        new.__curr__ = self.__curr__
-
-        index = key % new.len
-        new.keynumber -= 1
-        for value in new.data[index]:
-            if value == key:
-                new.data[index].remove(value)
-        return new
-
-    # 3. size
-    def size(self):
-        return self.len
-
-    # 4. key_number
-    def key_number(self):
-        return self.keynumber
-
-    # 5. is_member
-    def is_member(self, key):
-        for layer1 in self.data:
-            for layer2 in layer1:
-                if key == layer2:
-                    return True
-        return False
-
-    # 6. conversion
-    def from_list(self, list_A):
-        new = HashMap()
-        new.data = self.data
-        new.len = self.len
-        new.keynumber = self.keynumber
-        new.__curr__ = self.__curr__
-
-        for i in list_A:
-            new = new.add(i)
-        return new
-
-    def to_list(self):
-        list_A = []
-        for i in range(self.len):
-            for v in self.data[i]:
-                list_A.append(v)
-        return list(set(list_A))
-
-    # 7. filter: return a list
-    def filter(self, func):
-        res = []
-        for i in range(self.len):
-            for v in self.data[i]:
-                if func(v):
-                    res.append(v)
-        return res
-
-    # 8.map(func): return a list
-    def map(self, func):
-        res = []
-        for i in range(self.len):
-            for v in self.data[i]:
-                res.append(func(v))
-        return res
-
-    # 9.reduce: return accumulator
-    def reduce(self, func, initial):
-        accumulator = initial
-        for i in range(self.len):
-            for v in self.data[i]:
-                accumulator = func(accumulator, v)
-        return accumulator
-
-    # 10.monoid_add
-    def monoid_add(self, another_hash):
-        if another_hash.keynumber == 0:
-            return self
-
-        new = HashMap()
-        new.data = self.data
-        new.len = self.len
-        new.keynumber = self.keynumber
-        new.__curr__ = self.__curr__
-
-        for i in range(another_hash.len):
-            for v in another_hash.data[i]:
-                if not new.is_member(v):
-                    new = new.add(v)
-        new.keynumber += another_hash.keynumber
-        return new
 
     # 11.iteration and __next__
     def __iter__(self):
@@ -137,3 +30,138 @@ class HashMap(object):
                 raise StopIteration
         else:
             raise StopIteration
+
+
+# 1. cons
+def cons(key, hashmap):
+    new = HashMap()
+    new.data = copy.deepcopy(hashmap.data)
+    new.len = hashmap.len
+    new.keynumber = hashmap.keynumber
+    new.__curr__ = hashmap.__curr__
+
+    if member(key, hashmap):
+        return new
+
+    if key is None:
+        index = 0
+    else:
+        index = key % new.len
+    new.data[index].append(key)
+    new.keynumber += 1
+    return new
+
+
+# 2. remove
+def remove(hashmap, key):
+    new = HashMap()
+    new.data=copy.deepcopy(hashmap.data)
+    new.len = hashmap.len
+    new.keynumber = hashmap.keynumber
+    new.__curr__ = hashmap.__curr__
+
+    if key is None:
+        index = 0
+    else:
+        index = key % new.len
+    new.keynumber -= 1
+    for value in new.data[index]:
+        if value == key:
+            new.data[index].remove(value)
+    return new
+
+
+# 3. length
+def length(hashmap):
+    return hashmap.keynumber
+
+
+# 4. member
+def member(key, hashmap):
+    for layer1 in hashmap.data:
+        for layer2 in layer1:
+            if key == layer2:
+                return True
+    return False
+
+
+# 5. intersection
+def intersection(hashmap1, hashmap2):
+    new = HashMap()
+    set1 = set()
+    for layer1 in hashmap1.data:
+        for layer2 in layer1:
+            set1.add(layer2)
+    set2 = set()
+    for layer1 in hashmap2.data:
+        for layer2 in layer1:
+            set2.add(layer2)
+    set3 = set1.intersection(set2)
+    for v in set3:
+        new = cons(v, new)
+    return new
+
+
+# 6. conversion
+def from_list(list_A):
+    new = HashMap()
+    for index, value in enumerate(list_A):
+        new = cons(value, new)
+    return new
+
+
+def to_list(hashmap):
+    list_A = []
+    for i in range(hashmap.len):
+        for v in hashmap.data[i]:
+            list_A.append(v)
+    return list(set(list_A))
+
+
+# 7. concat
+def concat(hashmap1, hashmap2):
+    new = HashMap()
+    list1 = to_list(hashmap1)
+    list2 = to_list(hashmap2)
+    list = list1 + list2
+    new = from_list(list)
+    return new
+
+
+# 8. filter: return a list
+def filter(hashmap, func):
+    res = []
+    for i in range(hashmap.len):
+        for v in hashmap.data[i]:
+            if func(v):
+                res.append(v)
+    return res
+
+
+# 9.map(func): return a list
+def map(hashmap, func):
+    res = []
+    for i in range(hashmap.len):
+        for v in hashmap.data[i]:
+            res.append(func(v))
+    return res
+
+
+# 10.reduce: return accumulator
+def reduce(hashmap, func, initial):
+    accumulator = initial
+    for i in range(hashmap.len):
+        for v in hashmap.data[i]:
+            accumulator = func(accumulator, v)
+    return accumulator
+
+
+# 11.monoid_add
+def monoid_add(hashmap, another_hash):
+    if another_hash.keynumber == 0:
+        return hashmap
+    list1 = to_list(hashmap)
+    list2 = to_list(another_hash)
+    list = list1 + list2
+    new = from_list(list)
+    return new
--- test_immutable.py
+++ test_immutable.py
@@ -1,7 +1,8 @@
 import unittest
 from hypothesis import given
 import hypothesis.strategies as st
-from immutable import HashMap
+from immutable import HashMap, cons, length, remove, member, intersection, \
+                      to_list, from_list, concat, map, filter,reduce, monoid_add
 
 
 class TestMutableHashMap(unittest.TestCase):
@@ -10,6 +11,42 @@ class TestMutableHashMap(unittest.TestCase):
 
 
 class TestMutableHashMapMethods(TestMutableHashMap):
+    def test_awpi(self):
+        empty = HashMap()
+        self.assertEqual(to_list(cons(None, empty)), [None])
+        l1 = cons(None, cons(1, empty))
+        l2 = cons(1, cons(None, empty))
+        self.assertEqual(to_list(empty), [])
+        self.assertTrue(to_list(l1) == [None, 1] or to_list(l1) == [1, None])
+        self.assertNotEqual(to_list(empty), to_list(l1))
+        self.assertNotEqual(to_list(empty), to_list(l2))
+        self.assertEqual(to_list(l1), to_list(l2))
+        self.assertEqual(to_list(l1), to_list(cons(None, cons(1, l1))))
+        self.assertEqual(length(empty), 0)
+        self.assertEqual(length(l1), 2)
+        self.assertEqual(length(l2), 2)
+        self.assertEqual(to_list(remove(l1, None)), [1])
+        self.assertEqual(to_list(remove(l1, 1)), [None])
+        self.assertFalse(member(None, empty))
+        self.assertTrue(member(None, l1))
+        self.assertTrue(member(1, l1))
+        self.assertFalse(member(2, l1))
+        self.assertEqual(to_list(intersection(l1, l2)), to_list(l1))
+        self.assertEqual(to_list(intersection(l1, l2)), to_list(l2))
+        self.assertEqual(to_list(intersection(l1, empty)), to_list(empty))
+        self.assertEqual(to_list(intersection(
+            l1, cons(None, empty))), to_list(cons(None, empty)))
+        self.assertTrue(to_list(l1) == [None, 1] or to_list(l1) == [1, None])
+        self.assertEqual(to_list(l1), to_list(from_list([None, 1])))
+        self.assertEqual(to_list(l1), to_list(from_list([1, None, 1])))
+        self.assertEqual(to_list(concat(l1, l2)), to_list(from_list([None, 1, 1, None])))
+        lst = to_list(l1) + to_list(l2)
+        for e in l1:
+            lst.remove(e)
+        for e in l2:
+            lst.remove(e)
+        self.assertEqual(lst, [])
+
     def test_init(self):
         self.assertEqual(self.hashMap.len, 5)
         self.assertIsInstance(self.hashMap.data, list)
@@ -23,7 +60,7 @@ class TestMutableHashMapMethods(TestMutableHashMap):
     def test_add(self):
         new = HashMap()
         for i in range(5):
-            new = new.add(i)
+            new = cons(i, new)
         self.assertEqual(new.data[0], [0])
         self.assertEqual(new.data[1], [1])
         self.assertEqual(new.data[2], [2])
@@ -33,31 +70,27 @@ class TestMutableHashMapMethods(TestMutableHashMap):
     # 2. remove
     def test_remove(self):
         new = HashMap()
-        new = new.add(0)
+        new = cons(0, new)
         self.assertEqual(new.data[0], [0])
-        new = new.remove(0)
+        new = remove(new, 0)
         self.assertEqual(new.data[0], [])
 
-    # 3. size
-    def test_size(self):
-        self.assertEqual(self.hashMap.size(), 5)
+    # 3. length
+    def test_length(self):
+        self.assertEqual(length(self.hashMap), 0)
 
-    # 4. key_number
-    def test_key_num(self):
-        self.assertEqual(self.hashMap.key_number(), 0)
-
-    # 5. is_member
-    def test_is_member(self):
+    # 4. member
+    def test_member(self):
         new = HashMap()
         list_a = [0, 1, 2, 3, 4, 11]
-        new = new.from_list(list_a)
-        self.assertEqual(new.is_member(11), True)
-        self.assertEqual(new.is_member(9), False)
+        new = from_list(list_a)
+        self.assertEqual(member(11, new), True)
+        self.assertEqual(member(9, new), False)
 
     # 6. conversion
     def test_from_list(self):
         list_a = [0, 1, 2, 3, 4, 11]
-        self.hashMap.from_list(list_a)
+        self.hashMap = from_list(list_a)
         self.assertEqual(self.hashMap.data[0], [0])
         self.assertEqual(self.hashMap.data[1], [1, 11])
         self.assertEqual(self.hashMap.data[2], [2])
@@ -68,7 +101,7 @@ class TestMutableHashMapMethods(TestMutableHashMap):
     def test_filter(self):
         new = HashMap()
         list_a = [0, 1, 2, 3, 4, 11]
-        new.from_list(list_a)
+        new = from_list(list_a)
 
         def filterOdd(value):
             if value % 2 == 0:
@@ -76,31 +109,31 @@ class TestMutableHashMapMethods(TestMutableHashMap):
             else:
                 return False
 
-        res = new.filter(filterOdd)
+        res = filter(new, filterOdd)
         self.assertEqual(res, [0, 2, 4])
 
     # 8.map(func)
     def test_map(self):
         new = HashMap()
         list_a = [0, 1, 2, 3, 4, 11]
-        new.from_list(list_a)
+        new = from_list(list_a)
 
         def mapPlusOne(value):
             return value + 1
 
-        res = new.map(mapPlusOne)
+        res = map(new, mapPlusOne)
         self.assertEqual(res, [1, 2, 12, 3, 4, 5])
 
     # 9.reduce
     def test_reduce(self):
         new = HashMap()
         list_a = [0, 1, 2, 3, 4, 11]
-        new.from_list(list_a)
+        new = from_list(list_a)
 
         def reduceSum(accumulator, curr):
             return accumulator + curr
 
-        res = new.reduce(reduceSum, 0)
+        res = reduce(new, reduceSum, 0)
         self.assertEqual(res, 21)
 
     # The property - based tests
@@ -108,8 +141,8 @@ class TestMutableHashMapMethods(TestMutableHashMap):
     @given(st.lists(st.integers(5)))
     def test_from_list_to_list_equality(self, a):
         hash1 = HashMap()
-        hash1 = hash1.from_list(a)
-        b = hash1.to_list()
+        hash1 = from_list(a)
+        b = to_list(hash1)
         a = set(a)
         b = set(b)
         self.assertEqual(a, b)
@@ -118,8 +151,8 @@ class TestMutableHashMapMethods(TestMutableHashMap):
     def test_python_len_and_list_size_equality(self, a):
         a = list(set(a))
         hash1 = HashMap()
-        hash1 = hash1.from_list(a)
-        self.assertEqual(hash1.key_number(), len(a))
+        hash1 = from_list(a)
+        self.assertEqual(length(hash1), len(a))
 
     # 10.monoid_add
     def test_monoid_add(self):
@@ -127,26 +160,24 @@ class TestMutableHashMapMethods(TestMutableHashMap):
         hash2 = HashMap()
         list_a = [0, 1, 2, 3, 4, 11]
         list_b = [5, 6, 7]
-        hash1 = hash1.from_list(list_a)
-        hash2 = hash2.from_list(list_b)
-        tmp1 = hash1
-        tmp2 = hash2
+        hash1 = from_list(list_a)
+        hash2 = from_list(list_b)
         # list1 = hash1 add hash2
-        list1 = tmp1.monoid_add(hash2).to_list()
+        list1 = to_list(monoid_add(hash1, hash2))
         # list2 = hash2 add hash1
-        list2 = tmp2.monoid_add(hash1).to_list()
+        list2 = to_list(monoid_add(hash2, hash1))
         self.assertEqual(list1, list2)
 
     # 11.iteration and __next__
     def test_iter(self):
         x = [1, 2, 3]
         hash1 = HashMap()
-        hash1 = hash1.from_list(x)
+        hash1 = from_list(x)
         tmp = []
         for e in hash1:
             tmp.append(e)
         self.assertEqual(x, tmp)
-        self.assertEqual(hash1.to_list(), tmp)
+        self.assertEqual(to_list(hash1), tmp)
         i = iter(HashMap())
         self.assertRaises(StopIteration, lambda: next(i))
 
